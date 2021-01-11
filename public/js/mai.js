80c8924073e3fdb7e0d89ba6c7751b522516b955
Zakaria <zakPro@live.be>
2021-01-11 11:51:35 +0100

    exo-fini

---
+++ public/js/mai.js
@@ -0,0 +1,25 @@
+// EXERCICE 1
+
+// let nombre = prompt("indiquer un nombre");
+// let compteur = 0;
+
+// while (compteur <= nombre) {
+//     console.log(compteur);
+//     compteur++;
+// }
+
+
+// EXERCICE 2
+// let compt = 0;
+// while (compt <=3) {
+//     console.log(`${compt} son tour est de ${compt+1}`);
+//     compt++;
+// }
+
+let compt = 3;
+while (compt <=6) {
+    console.log(`${compt} son tour est de ${compt+1}`);
+
+    // ceci est le dernier point
+    compt = compt+2;  
+}
